322a92d53df05fd71498cad909cedb4195e14939
Andrzej Dubaj <dubaj.andrzej@gmail.com>
2018-05-23 17:34:23 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+let suits = ['Hearts', 'Clubs', 'Diamonds', 'Spaded'];
+let values = ['Ace', 'King', 'Queen', 'Jack', 'Ten', 'Nine', 'Eight', 'Seven', 'Six',
+  'Five', 'Four', 'Three', 'Two'
+];
+
+let deck = [];
